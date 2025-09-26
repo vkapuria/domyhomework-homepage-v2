e97ecf2c6e0c0606d522be9387a2143703c6a6a0
vkapuria <v.kapuria@gmail.com>
2025-09-26 20:27:55 +0530

    chore: disable react/no-unescaped-entities and relax unused-vars rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,10 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    rules: {
+      "react/no-unescaped-entities": "off", // ✅ allow ' and "
+      "@typescript-eslint/no-unused-vars": "warn", // optional: downgrade unused vars to warnings
+    },
   },
 ];
 
